3f1e2c7da99d9a44e9d9a8cda1ae80354a755739
J Kyle Armstrong <j.kyle.armstrong@gmail.com>
2023-06-02 03:55:54 -0400

    added table example

--- generate-example-code.R
+++ generate-example-code.R
@@ -1,4 +1,5 @@
-library(purrr)
+library('tidyverse')
+library('purrr')
 
 generate_code <- function(x) {
   code <- paste(
@@ -19,6 +20,37 @@ codes <- map(11:100, generate_code)
 
 # Write codes to file
 con <- file("output.txt", "w")
+for (code in codes) {
+  writeLines(code, con)
+}
+close(con)
+
+
+
+#########################
+
+generate_table <- function(x) {
+  code <- paste(
+    "```{r}", "\n",
+    "#| label: tbl-", x, "\n",
+    "#| tbl-cap: \"Sample ", x, "\"\n\n",
+    "tibble::enframe(Sys.info()) %>% \n",
+    "  filter(name %in% c('sysname','release','version','machine')) %>%", "\n",
+    "  knitr::kable()", "\n",
+    "```",
+    sep = ""
+  )
+  return(code)
+}
+
+codes <- map(1:100, generate_table)
+
+if (file.exists("eleven.qmd")) {
+  file.remove("eleven.qmd")
+}
+
+
+con <- file("eleven.qmd", "w")
 for (code in codes) {
   writeLines(code, con)
 }
